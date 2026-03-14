7c3f31a63b04526ba110390d6ea4448b481a5c55
nowowc1024 <cn150891@student.sgh.waw.pl>
2026-03-14 22:43:33 +0000

    Complie and Run

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+public class App {
+public static void main(String[] args) {
+Greeter greeter = new Greeter("Hello");
+String who = (args.length > 0) ? args[0] : "world";
+System.out.println(greeter.greet(who));
+}
+}
